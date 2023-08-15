fc83c4689d3cc6a819e536c286e10a2e3c85ad38
Luka Kureljušić <lkureljusic@ymail.com>
2023-08-15 20:43:56 +0200

    interceptor2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import MyRouter from "./router/MyRouter";
-
+import './helpers/interceptors/TokenInterceptor'
 import "./static/css/linearicons.css";
 import "./static/css/nice-select.css";
 
