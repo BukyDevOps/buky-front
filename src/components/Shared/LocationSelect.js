ad9058547b4b4fb215b7955714e8561e38e80e8e
Luka Kureljušić <lkureljusic@ymail.com>
2023-08-15 19:13:07 +0200

    del alert

--- src/components/Shared/LocationSelect.js
+++ src/components/Shared/LocationSelect.js
@@ -35,7 +35,6 @@ export const LocationSelect = ({ setLocation }) => {
     };
 
     setLocation(newLocation);
-    alert(JSON.stringify(newLocation))
     setSuggestions([]);
     setAddress(properties.formatted);
   };
